474be8a56b1a6ae1a79219c63536167e8c5ece6f
varyasmitt <varyasmitt@icloud.com>
2022-02-26 13:12:05 +0300

    first commit

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,13 +1,12 @@
 package ru.netology.sqr;
 
 public class SQRService {
-    public int sqrValuesAreIncluded(int up, int down, int firstValue, int secondValue) {
+    public int sqrValuesAreIncluded(int up, int down) {
         int number = 0;
-        for (int i = firstValue; i <= secondValue; i++) {
-            if (down <= i * i && i * i <= up)
-                 {
-                    number++;
-                }
+        for (int i = 10; i <= 99; i++) {
+            if (down <= i * i && i * i <= up) {
+                number++;
+            }
         }
         return number;
     }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -8,14 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"'Three Values Are Included',300,200,10,99,3",
-            "'All Values Are Included',10000,100,10,99,90",
-            "'Zero Values Are Included', 99,0,10,99,0"})
-    void shouldSqrValuesAreIncluded(String testName, int up, int down, int firstValue, int secondValue, int expected) {
+    @CsvSource(value = {"'Three Values Are Included',300,200,3",
+            "'All Values Are Included',10000,100,90",
+            "'Zero Values Are Included', 99,0,0"})
+    void shouldSqrValuesAreIncluded(String testName, int up, int down, int expected) {
         SQRService service = new SQRService();
 
 
-        int actual = service.sqrValuesAreIncluded(up, down, firstValue, secondValue);
+        int actual = service.sqrValuesAreIncluded(up, down);
 
         assertEquals(expected, actual);
     }
